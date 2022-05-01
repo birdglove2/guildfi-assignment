4f48d349091e72c29abf38661ce16940eebb5430
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-01 18:45:35 +0700

    :white_check_mark: create user route test

--- backend/user/src/routes/__test__/create.test.ts
+++ backend/user/src/routes/__test__/create.test.ts
@@ -1,7 +1,20 @@
-import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { natsWrapper } from '../../nats-wrapper';
 
+const dummyUserAttrs = global.dummyUserAttrs;
+
 it('should create user and emit an event successfully', async () => {});
-it('should not create user if the user already exists', async () => {});
+
+it('should not create user if the user already exists', async () => {
+  const res = await request(app).post('/api/user').send(dummyUserAttrs);
+
+  expect(res.status).toBe(201);
+
+  const res2 = await request(app).post('/api/user').send(dummyUserAttrs);
+  expect(res2.status).toBe(400);
+
+  // console.log(res2.body);
+  //TODO: standardize success and fail response
+  // expect(res2.body.errors[0].message).toBe('User already exists!');
+});
