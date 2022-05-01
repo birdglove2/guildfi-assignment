43545411edc9beab53fe44452df03f7d9ad6062d
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-01 19:39:39 +0700

    :sparkles: test show a user route

--- backend/user/src/routes/__test__/show.test.ts
+++ backend/user/src/routes/__test__/show.test.ts
@@ -1,4 +1,19 @@
-it('should return the detail of user');
+import request from 'supertest';
+import { app } from '../../app';
 
-// maybe ?
-it('should not return some private details if unauthorized');
+const dummyUserAttrs = global.dummyUserAttrs;
+
+it('should return the detail of user', async () => {
+  await request(app).post('/api/user').send(dummyUserAttrs).expect(201);
+
+  const res = await request(app)
+    .get(`/api/user/${dummyUserAttrs.address}`)
+    .send(dummyUserAttrs)
+    .expect(200);
+
+  expect(res.body.result.address).toBe(dummyUserAttrs.address);
+  expect(res.body.result.name).toBe(dummyUserAttrs.name);
+});
+
+// TODO: for further functionality ?
+it('should not return some private details if unauthorized', async () => {});
