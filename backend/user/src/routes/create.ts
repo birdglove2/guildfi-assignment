0a8e8170515dd4c943d263df4f04dc4e9f4225cb
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-01 18:43:02 +0700

    :sparkles: create a user route

--- backend/user/src/routes/create.ts
+++ backend/user/src/routes/create.ts
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { validateRequest, NotFoundError, BadRequestError } from 'common';
+import { validateRequest, NotFoundError, BadRequestError } from '../common';
 import { body } from 'express-validator';
-import { UserCreatedPublisher } from '/events/publishers/user-created-publisher';
+import { User } from '../models/user';
 import { natsWrapper } from '../nats-wrapper';
+import { UserCreatedPublisher } from '../events/publishers/user-created-publisher';
 
 const router = express.Router();
 
@@ -13,11 +14,34 @@ router.post(
     body('address')
       .not()
       .isEmpty()
-      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      // TODO: verify valid address
+      // .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
       .withMessage('address must be provided'),
+    body('name').not().isEmpty().withMessage('name must be provided'),
   ],
   validateRequest,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { address, name } = req.body;
+
+    const user = await User.find({ address: address });
+    if (user.length > 0) {
+      throw new BadRequestError('User already exists!');
+    }
+
+    const newUser = User.build({
+      address,
+      name,
+    });
+    await newUser.save();
+    new UserCreatedPublisher(natsWrapper.client).publish({
+      id: newUser.id,
+      address: newUser.address,
+      name: newUser.name,
+      version: newUser.version,
+    });
+
+    res.status(201).send({ success: true, result: newUser });
+  }
 );
 
 export { router as createUserRouter };
