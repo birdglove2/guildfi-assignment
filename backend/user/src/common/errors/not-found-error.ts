c60def25c7070e758ae5e6c592784130b5d6cc3c
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-01 19:31:05 +0700

    :recycle: NotFoundError

--- backend/user/src/common/errors/not-found-error.ts
+++ backend/user/src/common/errors/not-found-error.ts
@@ -3,13 +3,12 @@ import { CustomError } from './custom-error';
 export class NotFoundError extends CustomError {
   statusCode = 404;
 
-  constructor() {
-    super('Route not found');
-
+  constructor(public message: string) {
+    super(message);
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 
   serializeErrors() {
-    return [{ message: 'Not Found' }];
+    return [{ message: `${this.message} Not Found` }];
   }
 }
