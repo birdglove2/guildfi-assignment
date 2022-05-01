f1446f3bd10aaf65a51896152a36bf1f3c1fbca5
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-01 18:43:16 +0700

    :sparkles: show a user route

--- backend/user/src/routes/show.ts
+++ backend/user/src/routes/show.ts
@@ -1,8 +1,20 @@
 import express, { Request, Response } from 'express';
-import { NotFoundError, NotAuthorizedError } from 'common';
+import { NotFoundError, NotAuthorizedError } from '../common';
+import { User } from '../models/user';
 
 const router = express.Router();
 
-router.get('/api/user/:address', async (req: Request, res: Response) => {});
+router.get('/api/user/:address', async (req: Request, res: Response) => {
+  const { address } = req.params;
+
+  const user = await User.find({ address: address });
+  if (!user) {
+    throw new NotFoundError();
+  }
+
+  // TODO: query GEM balance
+
+  res.status(200).json({ success: true, result: user });
+});
 
 export { router as showUserRouter };
