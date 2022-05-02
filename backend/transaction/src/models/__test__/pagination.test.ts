efdac3464616eab3187ea46de80eb54ecee755f8
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-02 21:09:09 +0700

    :sparkles: pagination test

---
+++ backend/transaction/src/models/__test__/pagination.test.ts
@@ -0,0 +1,53 @@
+import { Pagination } from '../pagination';
+
+it('should return correct pagination', () => {
+  let pagination = Pagination.create(1, 10, 10);
+  expect(pagination).toEqual({
+    page: 1,
+    limit: 10,
+    prevPage: 1,
+    nextPage: 1,
+    totalPage: 1,
+    totalItems: 10,
+  });
+
+  pagination = Pagination.create(10, 1, 10);
+  expect(pagination).toEqual({
+    page: 10,
+    limit: 1,
+    prevPage: 9,
+    nextPage: 10,
+    totalPage: 10,
+    totalItems: 10,
+  });
+
+  pagination = Pagination.create(10, 10, 100);
+  expect(pagination).toEqual({
+    page: 10,
+    limit: 10,
+    prevPage: 9,
+    nextPage: 10,
+    totalPage: 10,
+    totalItems: 100,
+  });
+
+  pagination = Pagination.create(5, 4, 100);
+  expect(pagination).toEqual({
+    page: 5,
+    limit: 4,
+    prevPage: 4,
+    nextPage: 6,
+    totalPage: 25,
+    totalItems: 100,
+  });
+
+  pagination = Pagination.create(10, 10, 10);
+  expect(pagination).toEqual({
+    page: 1,
+    limit: 10,
+    prevPage: 1,
+    nextPage: 1,
+    totalPage: 1,
+    totalItems: 10,
+  });
+});
