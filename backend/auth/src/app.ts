952ac2f249b1121d3750d84e5bb4c12dbb3763b3
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-04 02:16:16 +0700

    :construction: auth service infra

---
+++ backend/auth/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { errorHandler, NotFoundError } from './common';
+import cors from 'cors';
+
+import { AuthRouter } from './routes';
+
+const app = express();
+app.use(cors());
+app.set('trust proxy', true);
+app.use(json());
+
+app.use('/api/v1/auth', AuthRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError('Route');
+});
+
+app.use(errorHandler);
+
+export { app };
