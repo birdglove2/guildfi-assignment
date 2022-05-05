4d630bddaed38cdfe729ebe5bca866c049238ede
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-05 23:54:01 +0700

    :recycle: re connectWallet

--- backend/user/src/api/controller.ts
+++ backend/user/src/api/controller.ts
@@ -1,34 +1,39 @@
 import { NextFunction, Request, Response } from 'express';
-import {
-  BadRequestError,
-  NotAuthorizedError,
-  NotFoundError,
-  successResponse,
-} from '@gfassignment/common';
+import { BadRequestError, successResponse } from '@gfassignment/common';
 import { UserService } from './service/user';
 import { WalletAddress } from './service/walletAddress';
 
 export class UserController {
   public static async createUser(req: Request, res: Response, next: NextFunction) {
     try {
-      if (!req.currentCredentials) {
-        throw new NotAuthorizedError();
-      }
+      const { name } = req.body;
 
-      const { signature, walletAddress, name } = req.body;
+      const user = await UserService.createUser({
+        authId: req.currentCredentials!.authId,
+        email: req.currentCredentials!.email,
+        name,
+      });
+
+      return successResponse(res, 201, user);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  public static async connectWallet(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { signature, walletAddress } = req.body;
       const isWalletOwner = await WalletAddress.verify(signature, walletAddress);
       if (!isWalletOwner) {
         throw new BadRequestError('Signature is not matched with wallet address provided');
       }
 
-      const user = await UserService.createUser({
-        authId: req.currentCredentials.authId,
-        email: req.currentCredentials.email,
+      const user = await UserService.updateUser({
+        authId: req.currentCredentials!.authId,
         walletAddress,
-        name,
       });
 
-      return successResponse(res, 201, user);
+      successResponse(res, 204, user);
     } catch (err) {
       next(err);
     }
