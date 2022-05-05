ffbb1009ec3c61614bb5a01a906e9f039ab6680b
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-05 23:53:12 +0700

    :pencil2: fix user validator

--- backend/user/src/api/validator.ts
+++ backend/user/src/api/validator.ts
@@ -1,6 +1,10 @@
 import { body } from 'express-validator';
 
 export const createUserValidator = [
+  body('name').not().isEmpty().withMessage('name must be provided'),
+];
+
+export const connectWalletValidator = [
   body('signature').not().isEmpty().withMessage('Signature must be provided'),
   body('walletAddress')
     .not()
@@ -8,5 +12,4 @@ export const createUserValidator = [
     // TODO: verify valid address
     // .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
     .withMessage('Wallet Address must be provided'),
-  body('name').not().isEmpty().withMessage('name must be provided'),
 ];
