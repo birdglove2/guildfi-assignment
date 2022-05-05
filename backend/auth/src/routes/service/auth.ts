6544fcf65039c07e7c0a877ef8bcea1fcb652493
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-05 15:55:24 +0700

    :sparkles: AuthService

---
+++ backend/auth/src/routes/service/auth.ts
@@ -0,0 +1,36 @@
+import { AuthAttrs } from 'models/auth';
+import { AuthRepository } from '../repository/auth';
+import { BadRequestError } from '@gfassignment/common';
+import { Password } from './password';
+
+export class AuthService {
+  public static async createUser(authAttrs: AuthAttrs) {
+    const { email, password } = authAttrs;
+    const existingUser = await AuthRepository.findByEmail(email);
+
+    if (existingUser) {
+      throw new BadRequestError('Email is already in used!');
+    }
+
+    const hashedPassword = await Password.hash(password);
+    const newUser = await AuthRepository.createUser({ email, password: hashedPassword });
+
+    return newUser;
+  }
+
+  public static async findByCredentials(authAttrs: AuthAttrs) {
+    const { email, password } = authAttrs;
+
+    const existingUser = await AuthRepository.findByEmail(email);
+    if (!existingUser) {
+      throw new BadRequestError('Invalid email or password');
+    }
+
+    const isPasswordMatched = await Password.compare(existingUser.password, password);
+    if (!isPasswordMatched) {
+      throw new BadRequestError('Invalid email or password');
+    }
+
+    return existingUser;
+  }
+}
