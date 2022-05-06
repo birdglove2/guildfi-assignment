8bd6261885dfa58157cda6458b1518c443a41e35
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-06 21:42:07 +0700

    :sparkles: transacitonRecord repo

---
+++ backend/transaction/src/api/repository/transactionRecord.ts
@@ -0,0 +1,23 @@
+import { TransactionRecord, TransactionRecordAttrs } from 'models/transactionRecord';
+import { knex, TABLES } from 'db';
+import { Pagination } from 'api/service/pagination';
+
+export class TransactionRecordRepository {
+  public static async findByUserAuthId(userAuthId: string, page: number, limit: number) {
+    const totalItems = +(await knex(TABLES.TransactionRecord).where({ userAuthId }).count())[0]
+      .count;
+
+    const pagination = Pagination.create(page, limit, totalItems);
+
+    const transactions = await knex(TABLES.TransactionRecord)
+      .where({ userAuthId })
+      .offset((page - 1) * limit)
+      .limit(limit);
+
+    return { transactions, pagination };
+  }
+
+  public static async createRecord(transactionRecordAttrs: TransactionRecordAttrs) {
+    return await TransactionRecord.insert(transactionRecordAttrs);
+  }
+}
