67ce3779daf020ee818bb0cadbe52fd095aa382e
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-07 14:28:48 +0700

    :fire: delete unused files and code

--- auth/src/api/index.ts
+++ auth/src/api/index.ts
@@ -8,7 +8,6 @@ import {
   validateRequest,
   NotAuthorizedError,
 } from '@gfassignment/common';
-import { MESSAGE } from 'models/credential.old';
 import { AuthController } from './controller';
 
 const router = express.Router();
@@ -17,46 +16,42 @@ router.post('/signup', signupValidator, validateRequest, AuthController.signup);
 
 router.post('/login', loginValidator, validateRequest, AuthController.login);
 
-router.get('/message', async (req: Request, res: Response) => {
-  return successResponse(res, 200, { message: MESSAGE });
-});
-
 router.get('/currentuser', currentCredentials, async (req: Request, res: Response) => {
   return successResponse(res, 200, req.currentCredentials);
 });
 
-router.post(
-  '/verify',
-  loginValidator,
-  validateRequest,
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { signature, walletAddress } = req.body;
+// router.post(
+//   '/verify',
+//   loginValidator,
+//   validateRequest,
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const { signature, walletAddress } = req.body;
+//     const MESSAGE = "Hello from backend"
+//     try {
+//       const decodedAddress = ethers.utils.verifyMessage(MESSAGE, signature);
 
-    try {
-      const decodedAddress = ethers.utils.verifyMessage(MESSAGE, signature);
+//       if (decodedAddress.toUpperCase() !== walletAddress.toUpperCase()) {
+//         // to prevent different message return different wallet address
+//         throw new NotAuthorizedError();
+//       }
 
-      if (decodedAddress.toUpperCase() !== walletAddress.toUpperCase()) {
-        // to prevent different message return different wallet address
-        throw new NotAuthorizedError();
-      }
+//       const token = jwt.sign(
+//         {
+//           walletAddress,
+//         },
+//         process.env.JWT_KEY!,
+//         {
+//           expiresIn: '30d',
+//         }
+//       );
 
-      const token = jwt.sign(
-        {
-          walletAddress,
-        },
-        process.env.JWT_KEY!,
-        {
-          expiresIn: '30d',
-        }
-      );
-
-      return successResponse(res, 200, { walletAddress, token });
-    } catch (err) {
-      // should find a way to handle multiple case of error
-      // for now it would return 500
-      next(err);
-    }
-  }
-);
+//       return successResponse(res, 200, { walletAddress, token });
+//     } catch (err) {
+//       // should find a way to handle multiple case of error
+//       // for now it would return 500
+//       next(err);
+//     }
+//   }
+// );
 
 export { router as AuthRouter };
--- auth/src/models/credential.old.ts
+++ auth/src/models/credential.old.ts
@@ -1,7 +1,3 @@
-// message used to sign from frontend to prove identity of wallet address
-// this message must be the same as that of frontend
-export const MESSAGE = 'Hello from backend';
-
 export interface CredentialAttrs {
   walletAddress: string;
   signature: string;
