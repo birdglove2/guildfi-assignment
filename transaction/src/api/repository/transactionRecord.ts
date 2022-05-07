18134b68df7a17b503f9fac76eda156e2c26006c
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-07 21:42:06 +0700

    :bug: fix getTransactionByUserAuthId

--- transaction/src/api/repository/transactionRecord.ts
+++ transaction/src/api/repository/transactionRecord.ts
@@ -3,6 +3,16 @@ import { knex, TABLES } from 'db';
 import { Pagination } from 'api/service/pagination';
 
 export class TransactionRecordRepository {
+  public static async listTransaction(page: number, limit: number) {
+    const totalItems = +(await knex(TABLES.Transaction).count())[0].count;
+
+    const pagination = Pagination.create(page, limit, totalItems);
+    const transactions = await knex(TABLES.Transaction)
+      .offset((page - 1) * limit)
+      .limit(limit);
+    return { transactions, pagination };
+  }
+
   public static async findByUserAuthId(userAuthId: string, page: number, limit: number) {
     const totalItems = +(await knex(TABLES.TransactionRecord).where({ userAuthId }).count())[0]
       .count;
